efffc4ad9dbabb001ae345944ba0414eb97de714
rutvikk05 <rutvikk5@gmail.com>
2022-10-29 17:32:51 +0530

    updated app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Profile from './components/Profile';
+import Links from './components/Links';
+import Social from './components/Social';
+import Footer from './components/Footer';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Profile />
+        <Links />
+        <Social />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
