6c2dd07527f0c420bd5f9a9f4aa99ccaea125400
rutvikk05 <rutvikk5@gmail.com>
2022-10-29 17:32:34 +0530

    updated all socials

---
+++ src/components/Social.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <section className='social'>
+        <img src="images/slack.svg" alt="slack icon" />
+        <a href="https://github.com/rutvikk05/linktree-zuri">
+          <img src="images/github.svg" alt="Github icon" />
+          </a>
+    </section>
+  )
+}
+
+export default Social;
